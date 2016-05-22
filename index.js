9ff804967d0617fda6ed88f7f9dd57bab942a5a4
Alejandro <alejanm09@gmail.com>
2016-05-22 15:56:25 +0200

    subidos

---
+++ index.js
@@ -0,0 +1,59 @@
+jQuery(document).ready(function() { 
+
+    $("#formRepo").hide();
+    $("#BTNescribir").hide();
+    $("#formFichero").hide();
+    $("#CogerToken").click(cogerToken);
+    $("#CogerRepo").click(cogerRepo);
+    $("#BTNescribir").click(function() {
+        $("#formFichero").show();
+    });
+    $("#CogerTexto").click(escribirFichero);
+});
+
+var github;
+var repo;
+
+function cogerToken(){
+    var token = $("#token").val();
+    github = new Github({
+        token: token,
+        auth: "oauth"
+    });
+    $("#formRepo").show();
+}
+
+function cogerRepo(){
+    var username = $("#NombreUsuario").val();
+    var reponame = $("#NombreRepo").val();
+    repo = github.getRepo(username, reponame);
+    repo.show(mostrarRepo);
+}
+
+function mostrarRepo(err, repo) {
+    var infoRepo = $("#infoRepo");
+    if(err){
+        infoRepo.html("Ha habido un error!<br>"+err);
+    }else{
+        infoRepo.html("<p>Datos del repositorio:</p><ul><li>Descripción del repositorio: " + repo.description + "</li>" + "<li>Fecha de creación: " + repo.created_at + "</li>" + "<li>Nombre completo: " + repo.full_name + "</li></ul>")
+    }
+    $("#BTNescribir").show();
+}
+
+function escribirFichero() {
+    var dict1 = {numero1: "hola", numero2: "hola"};
+    var dict2 = {numero1: "hola", numero2: "hola"};
+    var dict3 = {primero: dict1, segundo: dict2};
+    var nombreFichero = $("#NombreFichero").val();
+    var contenidoFichero = JSON.stringify(dict3);
+    var mensajeCommit = $("#MensajeCommit").val();
+    console.log(nombreFichero);
+    console.log(contenidoFichero);
+    console.log(mensajeCommit);
+    repo.write('master', nombreFichero, contenidoFichero, mensajeCommit,                function(err) {
+        if(!err){
+            $("#formFichero").html($("#formFichero").html() + "<p>Realizado con éxito.</p>");
+        }else{
+            $("#formFichero").html($("#formFichero").html() + "<p>error:" + err + "</p>");
+    }});
+};
